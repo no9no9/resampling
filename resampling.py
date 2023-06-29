d58e1209035da49a3ae931cb3a671e473be47d80
no9no9 <ysutou999@gmail.com>
2023-06-30 01:17:09 +0900

    change def name

--- resampling.py
+++ resampling.py
@@ -5,17 +5,15 @@ import soundfile
 import argparse
 import tqdm
 
-
-def resample(files_list,outdir_path,out_sr,out_dtype):
+def resampling(files_list,outdir_path,out_sr,out_dtype='PCM_16', add=''):
     files_num = len(files_list)
     for num,file in tqdm.tqdm(enumerate(files_list)):
         data, sr = librosa.load(file,sr=None,mono=True)
         data = librosa.resample(y=data,orig_sr=sr, target_sr=out_sr)
-        
         file_name = os.path.splitext(os.path.basename(file))[0]
-        write_path = os.path.join(outdir_path,'{}_resample.wav'.format(file_name))
+        write_path = os.path.join(outdir_path,f'{file_name}{add}.wav')
         soundfile.write(write_path,data,out_sr,subtype=out_dtype)
-        print(f'{num}/{files_num}: {write_path}')
+        print(f'\r{num}/{files_num}: {write_path}', end='')
     return 0
 
 def files_to_list(files_path):
@@ -27,20 +25,22 @@ def files_to_list(files_path):
 if __name__=="__main__":
     parse = argparse.ArgumentParser()
     parse.add_argument('-f', '--files_list',type=str, required=True)
-    parse.add_argument('-o', '--outdir_path',type=str, required=True)
-    parse.add_argument('-sr_out','--sampling_rate_output',type=int, required=True)
-    parse.add_argument('-d','--output_datatype' , type=str, default='PCM_16')
-    
+    parse.add_argument('-o', '--outdir_path',type=str, default='./resampling/output/', required=True)
+    parse.add_argument('-s','--sampling_rate_output',type=int, required=True)
+    parse.add_argument('-d','--output_dtype' , type=str, default='PCM_16')
+    parse.add_argument('-T','--text_add', type=str, default='')
     args = parse.parse_args()
     
-    if not os.path.isdir(args.outdir_path):
-        os.makedirs(args.outdir_path)
-        os.chmod(args.outdir_path,0o775)
-        
+    os.makedirs(args.outdir_path, exist_ok=True)
+    os.chmod(args.outdir_path,0o775)
+    
     files_list = files_to_list(args.files_list)
     outdir_path = args.outdir_path
+    sr = args.sampling_rate_output
+    dtype = args.output_dtype
+    text_add = args.text_add
     
-    resample(files_list, outdir_path,args.sampling_rate_output,args.output_datatype)
+    resampling(files_list, outdir_path, sr, dtype, text_add)
     
 
 
